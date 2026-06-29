91745014049eff873163d26b6615bbba903f5cee
Amanda <cestari.toronto@gmail.com>
2026-06-29 10:45:46 -0400

    added hum and changed typing

--- js/app.js
+++ js/app.js
@@ -12,7 +12,9 @@ registerCommands(terminal);
 
 const bootSound = document.getElementById("boot-sound");
 const typeSound = document.getElementById("type-sound");
+const humSound = document.getElementById("hum-sound");
 if (bootSound) bootSound.volume = 0.5; // 0.0 (silent) to 1.0 (full volume)
 if (typeSound) typeSound.volume = 0.3; // 0.0 (silent) to 1.0 (full volume)
+if (humSound) humSound.volume = 0.5;   // 0.0 (silent) to 1.0 (full volume)
 
 terminal.start();
